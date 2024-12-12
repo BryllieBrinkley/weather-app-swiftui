a3c2b50bfb53fa91260f747d62bd01cb934e0ca4
BryllieBrinkley <jibryllbrink@yahoo.com>
2024-12-12 13:56:58 -0500

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Jibryll Brinkley on 12/12/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
