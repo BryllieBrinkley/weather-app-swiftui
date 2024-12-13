91d5f34a144c881ccb516bb8aed9a3214c986ac3
BryllieBrinkley <jibryllbrink@yahoo.com>
2024-12-13 17:30:45 -0500

    isNight mode complete

--- WeatherApp/ContentView.swift
+++ WeatherApp/ContentView.swift
@@ -1,24 +1,125 @@
-//
-//  ContentView.swift
-//  WeatherApp
-//
-//  Created by Jibryll Brinkley on 12/12/24.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var isNight = false
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            BackgroundView(isNight: isNight)
+            VStack {
+                CityTextView(cityName: "Cupertino, CA")
+                MainWeatherStatusView(imageName: isNight ? "moon.stars.fill" : "cloud.sun.fill",
+                                      temperature: isNight ? 57 : 76)
+                
+                HStack(alignment: .center, spacing: 25) {
+                    WeatherDayView(dayOfWeek: "TUE",
+                                   imageName: "cloud.sun.fill",
+                                   temperature: 74)
+                    WeatherDayView(dayOfWeek: "WED",
+                                   imageName: "sun.max.fill",
+                                   temperature: 88)
+                    WeatherDayView(dayOfWeek: "THU",
+                                   imageName: "wind",
+                                   temperature: 55)
+                    WeatherDayView(dayOfWeek: "FRI",
+                                   imageName: "sunset.fill",
+                                   temperature: 60)
+                    WeatherDayView(dayOfWeek: "SAT",
+                                   imageName: "moon.stars.fill",
+                                   temperature: 25)
+                }
+                Spacer()
+                
+                Button {
+                    isNight.toggle()
+                } label: {
+                    WeatherButton(title: "Change Time of Day",
+                                  textColor: .white,
+                                  backgroundColor: isNight ? .gray : .teal)
+                }
+                Spacer()
+            }
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
+}
+
+struct WeatherDayView: View {
+    
+    var dayOfWeek: String
+    var imageName: String
+    var temperature: Int
+    
+    var body: some View {
+        VStack {
+            Text(dayOfWeek)
+                .font(.system(size: 16, weight: .medium, design: .default))
+                .foregroundColor(.white)
+            Image(systemName: imageName)
+                .renderingMode(.original)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 40, height: 40)
+            
+            Text("\(temperature)°")
+                .font(.system(size: 28, weight: .medium))
+                .foregroundColor(.white)
+        }
+    }
+}
+
+struct BackgroundView: View {
+    
+    var isNight: Bool
+    
+    var body: some View {
+//        LinearGradient(gradient: Gradient(colors: [isNight ? . black : .blue, isNight ? .gray : Color("lightBlue")]),
+//                       startPoint: .topLeading,
+//                       endPoint: .bottomTrailing)
+//        .ignoresSafeArea(.all)
+        ContainerRelativeShape()
+            .fill(isNight ? Color.black.gradient : Color.blue.gradient)
+            .ignoresSafeArea(.all)
+        
+        
+        
+    }
+}
+
+struct CityTextView: View {
+    
+    var cityName: String
+    
+    var body: some View {
+        Text(cityName)
+            .font(.system(size: 32, weight: .medium, design: .default))
+            .foregroundColor(.white)
+            .padding()
+    }
+}
+
+struct MainWeatherStatusView: View {
+    
+    var imageName: String
+    var temperature: Int
+    
+    var body: some View {
+        VStack(alignment: .center,spacing: 10) {
+            Image(systemName: imageName)
+                .renderingMode(.original)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 180, height: 180)
+            Text("\(temperature)°")
+                .font(.system(size: 90, weight: .medium))
+                .foregroundColor(.white)
+        }
+        .padding(.bottom, 60)
+
+    }
 }
